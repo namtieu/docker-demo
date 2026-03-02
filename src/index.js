b4d97cfbeee4002df1009ecb7c403ec437ea68a3
namtieu <hoainam.tieu@gmail.com>
2026-03-02 23:10:36 +0700

    update ci.yml

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("CI/CD working!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
