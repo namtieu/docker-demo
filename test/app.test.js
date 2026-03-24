03eea61008c64915b268d73dd713b6452383f04c
namtieu <hoainam.tieu@gmail.com>
2026-03-24 11:37:31 +0700

    updaate testjs

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const express = require("supertest");
 const app = require("../src/index");
 
 describe("GET /", () => {
-    it("should return 'Hello, Docker!'", async () => {
+    it("should return 'Hello, Docker. Today is 24th of March 2026 !'", async () => {
         const res = await express(app).get("/");
 
         expect(res.statusCode).toEqual(200);
